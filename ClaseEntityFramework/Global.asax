57b9d9ad2302971a6c772e707ebb8001568c49d3
psanchez <psanchez@plus54.com>
2020-05-27 20:02:17 -0300

    commit inicial tabla producto, productoRepository

---
+++ ClaseEntityFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ClaseEntityFramework.MvcApplication" Language="C#" %>
---
+++ DAL/ProductoRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ProductoRepository
+    {
+        protected PracticaEFEntities ctx { get; set; }
+
+        public ProductoRepository()
+        {
+            ctx = new PracticaEFEntities();
+        }
+        public Producto ObtenerPorId(int id)
+        {
+            return ctx.Producto.Find(id);
+        }
+
+        public List<Producto> ObtenerTodos()
+        {
+            return ctx.Producto.ToList();
+        }
+
+        public void Crear(Producto prod)
+        {
+            ctx.Producto.Add(prod);
+
+            ctx.SaveChanges();
+        }
+
+        public void Eliminar(int id)
+        {
+            Producto prodAEliminar = ObtenerPorId(id);
+            if (prodAEliminar != null)
+            {
+                ctx.Producto.Remove(prodAEliminar);
+            }
+
+            ctx.SaveChanges();
+        }
+    }
+}
