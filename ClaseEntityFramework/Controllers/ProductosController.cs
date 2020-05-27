afcb26ab081bb79290fce916b31902b8099b819b
psanchez <psanchez@plus54.com>
2020-05-27 20:16:25 -0300

    agragando ProductosController y listado de productos (tira error)

---
+++ ClaseEntityFramework/Controllers/ProductosController.cs
@@ -0,0 +1,20 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaseEntityFramework.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            ProductoRepository prodRepository = new ProductoRepository();
+            List<Producto> productos = prodRepository.ObtenerTodos();
+            return View(productos);
+        }
+    }
+}
