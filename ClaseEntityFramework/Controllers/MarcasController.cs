86363561a9111269c4fd3cdabaf5708f88b02b75
psanchez <psanchez@plus54.com>
2020-06-03 21:39:08 -0300

    Agregando Marcas/Eliminar con borrado de productos de esa marca y desvinculandolo de los Locales que tuvieran esos productos

---
+++ ClaseEntityFramework/Controllers/MarcasController.cs
@@ -0,0 +1,31 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaseEntityFramework.Controllers
+{
+    public class MarcasController : Controller
+    {
+        MarcaRepository marcaRepository;
+
+        public MarcasController()
+        {
+            PracticaEFEntities ctx = new PracticaEFEntities();
+            marcaRepository = new MarcaRepository(ctx);
+        }
+        // GET: Marcas
+        public ActionResult Index()
+        {
+            return View(marcaRepository.ObtenerTodos());
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            marcaRepository.Eliminar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- DAL/MarcaRepository.cs
+++ DAL/MarcaRepository.cs
@@ -11,5 +11,25 @@ namespace DAL
         public MarcaRepository(PracticaEFEntities context) : base(context)
         {
         }
+
+        /// <summary>
+        /// Elimina la Marca, elimina todos los Productos de la Marca y también los remueve de los Locales
+        /// </summary>
+        /// <param name="id">id de la marca a eliminar</param>
+        public override void Eliminar(int id)
+        {
+            //obtengo los productos de la marca a eliminar
+            var productosDeMarca = ctx.Producto.Where(o => o.IdMarca == id);
+            foreach (Producto prod in productosDeMarca)
+            {
+                //remuevo estos productos de cada local que lo referencia
+                prod.Local.Clear();
+                //elimino el producto
+                ctx.Producto.Remove(prod);
+            }
+
+            //elimino la marca
+            base.Eliminar(id);
+        }
     }
 }
